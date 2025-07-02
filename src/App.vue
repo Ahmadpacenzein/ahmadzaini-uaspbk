e992f997a603f8521276b669f00e23d79436ed32
Ahmadpacenzein <ahmadpacen@gmail.com>
2025-07-02 23:06:12 +0700

    Add Font Awesome icons for improved UI; update package.json and main.js

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <router-link to="/product">Product</router-link>
         <router-link to="/aboutus">About Us</router-link>
         <router-link to="/history">History</router-link>
-        <router-link to="/keranjang">Keranjang</router-link>
+        <router-link to="/keranjang"><font-awesome-icon :icon="['fas', 'cart-shopping']" /></router-link>
         <!-- <router-link to="/artikel">Artikel</router-link> -->
       </div>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from './router';
 import eventBus from './eventBus';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser, faLock, fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faLock,fas.faCartShopping); // Menambahkan ikon cart-shopping
 
 const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);//untuk fontawesome icon
+
 app.config.globalProperties.$eventBus = eventBus;
 app.use(router).mount('#app');
