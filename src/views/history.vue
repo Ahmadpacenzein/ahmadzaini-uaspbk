ab0d292d5088d2bcea0407802929a65a9288a49b
Ahmadpacenzein <ahmadpacen@gmail.com>
2025-07-03 14:19:58 +0700

    Add product data to db.json; update history.vue and home.vue for product display and fetching

--- src/views/history.vue
+++ src/views/history.vue
@@ -1,3 +1,34 @@
 <template>
-    ini history
+  <div class="history-container">
+    <h1>History</h1>
+    <ul>
+      <li>Sample history item 1</li>
+      <li>Sample history item 2</li>
+      <li>Sample history item 3</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "History"
+};
+</script>
+
+<style scoped>
+.history-container {
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  color: #333;
+  margin-bottom: 15px;
+}
+
+ul {
+  list-style-type: disc;
+  padding-left: 20px;
+  color: #555;
+}
+</style>
